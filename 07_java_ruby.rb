933d76e24ead67f12d6495f119cf8015602a24f4
mauro <mauro_biazutti@hotmail.com>
2023-03-05 11:24:18 -0300

    "ajustes"

---
+++ 07_java_ruby.rb
@@ -0,0 +1,13 @@
+#7) Qual a principal diferença entre estes dois métodos?
+
+#Java
+# puplic booleal isEmpty(String s){
+# return s.length() == 0;
+# }
+#
+# Ruby
+# def empty?(s)
+#   s.size == 0
+# end
+
+#A função Java vai verificar se o tamanho da  String é igual a 0, e a função Ruby vai
--- 09_ruby_tipagem.rb
+++ 09_ruby_tipagem.rb
@@ -1,14 +1,3 @@
 #9) Quais as vantagens e desvantagens do ruby ser uma linguagem dinamicamente tipada?
 
-#Java
-# puplic booleal isEmpty(String s){
-# return s.length() == 0
-# }
-#
-# Ruby
-# def empty?(s)
-# return s.size == 0
-# end
 
-#Ambos os codigos executam a mesma coisa,
-# porem a sintax do Java é mais verbosa que a do Ruby
