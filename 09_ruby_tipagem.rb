d5ab7fe05d845859fdd0cd49ab487a1e556c43a9
mauro <mauro_biazutti@hotmail.com>
2023-03-07 21:32:57 -0300

    "exe_09"

--- 09_ruby_tipagem.rb
+++ 09_ruby_tipagem.rb
@@ -1,3 +1,5 @@
 #9) Quais as vantagens e desvantagens do ruby ser uma linguagem dinamicamente tipada?
 
 
+# As vantagens seria na praticidade e na velocidade de codificação, sem se preocupar muito em ficar escrevendo os tipos.
+# As desvantagens, podem esconder erros em tempo de compilação que somente se manifestarão em tempo de execução.
