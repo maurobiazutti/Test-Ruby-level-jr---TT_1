1db53044f85f5711333d6982982d0be792fdd3b2
Mauro <mauro_biazutti@hotmail.com>
2026-06-16 09:57:54 -0300

    feat: resolução exercicio 01_palindromo

--- exercicios/01_palindrome.rb
+++ exercicios/01_palindrome.rb
@@ -2,11 +2,27 @@
 # do contrário ela deve retornar falso. (palíndromos são palavras ou frases que podem ser
 # lidas da esquerda para a direita ou da direita para a esquerda.)
 
+
+##### Usando metodo reverse #######
 def palindromo
   puts "Digite uma Palavra"
-  p = gets.chomp
-  puts p == p.reverse
+  palavra = gets.chomp
+  puts palavra == palavra.reverse
 end
-palindromo
+# palindromo
 
+
+############# Usando Ruby Puro ###############
+arara = "arara"
+ruby = "ruby"
+
+def palindromo(strg)
+  invertido = ""
+  strg.each_char do |char|
+    invertido = char + invertido
+  end
+    puts strg == invertido
+end
+palindromo(arara)
+palindromo(ruby)
 
