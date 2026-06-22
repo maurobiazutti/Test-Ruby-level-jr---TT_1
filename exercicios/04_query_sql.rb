f5b7a9ea75769af758a890369c4d3b4c5fc49bfa
Mauro <mauro_biazutti@hotmail.com>
2026-06-22 20:05:20 -0300

    feat: resolução exercicio 04 criação do banco, tabelas, insert e querys

--- exercicios/04_query_sql.rb
+++ exercicios/04_query_sql.rb
@@ -1,13 +1,67 @@
-# 4) A partir das tabelas do banco de dados abaixo, escreva uma query sql que através de
-# join, retorne o nome completo, cpf, equipe e cargo dos colaboradores ordenando os por cpf.
+=begin 
+4) A partir das tabelas do banco de dados abaixo, escreva uma query sql que através de
+join, retorne o nome completo, cpf, equipe e cargo dos colaboradores ordenando os por cpf.
 
-# SELECT CONCAT(first_name,' ',last_name) AS "Nome completo",
-# cpf AS CPF,
-# teams.name AS Equipe,
-# job_titles.name AS Colaboradores
-# FROM employees
-# INNER JOIN job_titles
-# ON employees.job_title_id=job_titles.id
-# INNER JOIN teams
-# ON employees.team_id=teams.id
-# ORDER BY cpf;
+create table job_titles (
+	id serial primary key,
+	name varchar(100) not null
+);
+
+create table teams (
+	id serial primary key,
+	name varchar(50) not null
+);
+
+create table employees (
+	id serial primary key,
+	first_name varchar(100) not null,
+	last_name varchar(100) not null,
+	cpf varchar(14) not null,
+	team_id integer,
+	job_title_id integer,
+
+	constraint fk_employee_team
+		foreign key (team_id)
+		references teams(id)
+		on delete set null,
+
+	constraint fk_employee_job_title
+		foreign key (job_title_id)
+		references job_titles(id)
+		on delete restrict
+);
+
+-- Populando os Cargos
+INSERT INTO job_titles (id, name) VALUES 
+(1, 'Desenvolvedor back-endimpr'),
+(2, 'Desenvolvedor front-end'),
+(3, 'Encantador de clientes');
+
+-- Populando os Times
+INSERT INTO teams (id, name) VALUES 
+(1, 'TI'),
+(2, 'Suporte'),
+(3, 'Financeiro');
+
+-- Populando os Funcionários
+INSERT INTO employees (id, first_name, last_name, cpf, team_id, job_title_id) VALUES 
+(1, 'Pedro', 'Alves', '665.415.876.80', 2, 3),
+(2, 'Tiago', 'Nogueira', '032.336.130-75', 1, 1),
+(3, 'João', 'Neves', '461.636.517.23', 1, 2);
+
+
+-- QUERYS
+SELECT * FROM employees;
+SELECT * FROM teams;
+SELECT * FROM job_titles;
+
+SELECT CONCAT(E.first_name, ' ', E.last_name) AS nome_completo, 
+E.cpf AS CPF, 
+T.name AS equipe, 
+J.name AS cargo
+FROM employees E
+INNER JOIN teams T on E.team_id = T.id
+INNER JOIN job_titles J ON E.job_title_id = J.id
+ORDER BY E.cpf;
+
+=end
