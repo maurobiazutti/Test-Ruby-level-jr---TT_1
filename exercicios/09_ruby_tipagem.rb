4ee317b576eed981b23d29739a55c5bce50efa96
Mauro <mauro_biazutti@hotmail.com>
2026-06-22 17:09:03 -0300

    feat: resolução exercicio 09 vantagens e desvantagens da tipagem

--- exercicios/09_ruby_tipagem.rb
+++ exercicios/09_ruby_tipagem.rb
@@ -2,4 +2,5 @@
 
 
 # As vantagens seria na praticidade e na velocidade de codificação, sem se preocupar muito em ficar escrevendo os tipos.
+
 # As desvantagens, podem esconder erros em tempo de compilação que somente se manifestarão em tempo de execução.
