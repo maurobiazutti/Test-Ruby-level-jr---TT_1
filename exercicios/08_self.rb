c8e9e627c92d4b9ba648042e4ca0d364d6d95894
Mauro <mauro_biazutti@hotmail.com>
2026-06-22 17:05:19 -0300

    feat: resolução exercicio 08 significado de self class

--- exercicios/08_self.rb
+++ exercicios/08_self.rb
@@ -1,3 +1,18 @@
 # 8) Qual o significado do self nas classes ruby?
 
-# Na classe, o self será a própria classe.
+# Em Ruby, self faz referencia ao objeto que está executando o codigo naquele momento.
+
+# Self como Metodo de Class
+# Quando o self. vem antes do nome do metodo dentro de uma class, 
+# isso significa que o metodo pertence a class em si e não uma instâcia (objeto) criado por ela.
+
+# Ex:
+class Carro
+  # Um método de classe usando self
+  def self.descricao
+    "Eu sou a classe Carro, sirvo de molde para criar carros!"
+  end
+end
+
+# Você chama direto na Classe:
+puts Carro.descricao # Saída: Eu sou a classe Carro...
